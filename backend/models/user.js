95dafd63d0c8197edacdd6485f38593122247b83
Ankur Srivastava <ankurlkw@gmail.com>
2022-01-02 12:04:14 +0530

    readme

--- backend/models/user.js
+++ backend/models/user.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     profile: {
         type: String
     },
-    password: {
+    hashed_password: {
         type: String
     },
     salt: {
@@ -48,6 +48,32 @@ const userSchema = new mongoose.Schema({
         default: ""
     }
 }, {timestamp: true})
+
+userSchema.virtual('password')
+    .set(function(password){
+        this._password = password
+        // generate salt
+        this.salt = makeSalt()
+        // encrypt password
+        this.hashed_password = encryptPassword(password)
+    })
+    .get(function() {
+        return this._password
+    })
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if(!password) {
+            return ''
+        }
+        try {
+
+        } catch(err) {
+            console.error(err)
+            return ''
+        }
+    }
+}
 
 // convention is to have model name first letter in caps
 module.exports = mongoose.model('User', userSchema)
